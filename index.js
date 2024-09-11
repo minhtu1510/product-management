bdd8438db86836bc36d61d7763ed6cb812eb307c
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-09-11 16:33:41 +0700

    1.2.Xây dựng layout chung

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express= require ("express");
+
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
+const databse = require("./config/database");
+databse.connect();
+
+const routeAdmin = require("./routes/admin/index.route");
 const routeClient = require("./routes/client/index.route");
 
 app.set("views", "./views");
@@ -11,6 +16,7 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 
 routeClient.routeClient(app);
+routeAdmin.routeAdmin(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,9 +2,7 @@ const productRoute = require("./product.route");
 const homeRoute = require("./home.route");
 
 module.exports.routeClient = (app) => {
-  app.get("/", (req, res) => {
-    res.render("client/pages/home/index");
-  });
+  app.use("/", homeRoute);
 
   app.use("/products", productRoute);
 };
