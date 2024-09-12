b23246f8c74ac0c56929259e2dcff7ce92f0fa94
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-09-12 11:26:03 +0700

    Lấy data sản phẩm in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,12 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+  });
+
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
+    products: products,
   });
 };
