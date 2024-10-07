9e1e649523043a7d4b91d8ea563db02d7f3097fa
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-10-07 15:41:15 +0700

    Thêm middleware cho route private

---
+++ middlewares/admin/auth.middleware.js
@@ -0,0 +1,21 @@
+const { prefixAdmin } = require("../../config/system");
+const Account = require("../../models/account.model");
+
+module.exports.requireAuth = async (req, res, next) => {
+  if (!req.cookies.token) {
+    res.redirect(`/${prefixAdmin}/auth/login`);
+    return;
+  }
+
+  const user = await Account.findOne({
+    token: req.cookies.token,
+    deleted: false,
+    status: "active",
+  });
+  if (!user) {
+    res.clearCookie("token");
+    res.redirect(`/${prefixAdmin}/auth/login`);
+    return;
+  }
+  next();
+};
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,20 +4,29 @@ const productCategoryRoute = require("./product-category.route");
 const roleRoute = require("./role.route");
 const accountRoute = require("./account.route");
 const authRoute = require("./auth.route");
+const authMiddleware = require("../../middlewares/admin/auth.middleware");
 
 const systemConfig = require("../../config/system");
 
 module.exports.routeAdmin = (app) => {
   const PATH_ADMIN = `/${systemConfig.prefixAdmin}`;
-  app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute);
+  app.use(
+    `${PATH_ADMIN}/dashboard`,
+    authMiddleware.requireAuth,
+    dashboardRoute
+  );
 
-  app.use(`${PATH_ADMIN}/products`, productRoute);
+  app.use(`${PATH_ADMIN}/products`, authMiddleware.requireAuth, productRoute);
 
-  app.use(`${PATH_ADMIN}/products-category`, productCategoryRoute);
+  app.use(
+    `${PATH_ADMIN}/products-category`,
+    authMiddleware.requireAuth,
+    productCategoryRoute
+  );
 
-  app.use(`${PATH_ADMIN}/roles`, roleRoute);
+  app.use(`${PATH_ADMIN}/roles`, authMiddleware.requireAuth, roleRoute);
 
-  app.use(`${PATH_ADMIN}/accounts`, accountRoute);
+  app.use(`${PATH_ADMIN}/accounts`, authMiddleware.requireAuth, accountRoute);
 
   app.use(`${PATH_ADMIN}/auth`, authRoute);
 };
