f59899a01653ca9768bbcb79fef5128691beec71
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-10-07 15:19:24 +0700

    Làm trang đăng nhập

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const productRoute = require("./product.route");
 const productCategoryRoute = require("./product-category.route");
 const roleRoute = require("./role.route");
 const accountRoute = require("./account.route");
+const authRoute = require("./auth.route");
 
 const systemConfig = require("../../config/system");
 
@@ -17,4 +18,6 @@ module.exports.routeAdmin = (app) => {
   app.use(`${PATH_ADMIN}/roles`, roleRoute);
 
   app.use(`${PATH_ADMIN}/accounts`, accountRoute);
+
+  app.use(`${PATH_ADMIN}/auth`, authRoute);
 };
