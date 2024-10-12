0ef372fdca6121c6e364b15fa52387f87c28f3f1
minhtu1510 <thaithiminhtu.338@gmail.com>
2024-10-13 00:17:21 +0700

    Hiên thị menu chung bên client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
 const productRoute = require("./product.route");
 const homeRoute = require("./home.route");
 
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+
 module.exports.routeClient = (app) => {
+  app.use(categoryMiddleware.category);
+
   app.use("/", homeRoute);
 
   app.use("/products", productRoute);
